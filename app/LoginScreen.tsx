f33fca78ff7ef4fa132d6d62a07a9b133c6c4494
paltansjosess <paltansjosess@gmail.com>
2025-11-11 03:03:44 +0000

    ✨ feat(login): integrar validaciones Zod al Login Se agrega el loginSchema que hicimos antes en LoginScreen AHora>     Muestra mensajes de error debajo de los campos si el usuario escribe mal.     Evita que el formulario se envíe si hay errores.

--- app/LoginScreen.tsx
+++ app/LoginScreen.tsx
@@ -1,13 +1,34 @@
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, Alert } from "react-native";
 import React ,{ useState }from "react";
 import { router } from "expo-router";
 import CustomInput from "../components/CustomInput";
+import { LoginSchema } from "@/lib/schemas/LoginSchema";
+import { z } from "zod";
 
 
 export default function LoginScreen() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>({});
+
+  const handleLogin = () => {
+    const result = LoginSchema.safeParse({ email, password });
+
+    if (!result.success) {
+  
+      const formErrors: any = {};// Si hay errores
+      result.error.errors.forEach((err) => {
+        const field = err.path[0];
+        formErrors[field] = err.message;// se muesta en los CustomInput
+      });
+      setErrors(formErrors);
+      return;
+    }
+
+    setErrors({});
+    Alert.alert("Login exitoso", `Bienvenido ${email}`);
+  };
 
   return (
     <View className="flex-1 justify-center items-center bg-gray-100 p-6">
@@ -18,6 +39,7 @@ export default function LoginScreen() {
         placeholder="ejemplo@gmail.com"
         value={email}
         onChangeText={setEmail}
+        error={errors.email}
       />
       <CustomInput
         label="Contraseña"
@@ -25,7 +47,13 @@ export default function LoginScreen() {
         value={password}
         onChangeText={setPassword}
         secureTextEntry
+        error={errors.password}
       />
+
+      <TouchableOpacity onPress={handleLogin}
+        className="bg-[#F54927] px-6 py-3 rounded-2xl mt-3">
+        <Text className="text-white font-semibold text-lg">START</Text>
+      </TouchableOpacity>
 
       <TouchableOpacity onPress={() => router.push("/RegisterScreen")}>
         <Text className="text-[#F54927] mt-4">No Account? Register. </Text>
